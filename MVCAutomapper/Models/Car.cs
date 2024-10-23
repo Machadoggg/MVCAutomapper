390c5e43e1163e247ece566cd78f82f67dbc3772
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-10-22 19:21:47 -0500

    Add project files.

---
+++ MVCAutomapper/Models/Car.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCAutomapper.Models
+{
+    public class Car
+    {
+        public string Brand { get; set; } = default!;
+        public int Power { get; set; }
+        public string Model { get; set; } = default!;
+        public decimal Price { get; set; }
+    }
+}
---
+++ MVCAutomapper/Models/CarRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace MVCAutomapper.Models
+{
+    public class CarRequest
+    {
+        public string Brand { get; set; } = default!;
+        public int Power { get; set; }
+    }
+}
